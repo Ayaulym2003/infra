a7aa81ca4d3cf6b7c04d88d1f215d25da839e0b3
Аяулым <aaulym@MacBook-Pro-Aaulym.local>
2021-12-25 05:37:05 +0600

    1

--- jscript.js
+++ jscript.js
@@ -30,3 +30,34 @@ $(document).ready(function(){
     po.style.marginTop = value * -0.18 + 'px';
     
     })    
+    let ew = document.getElementById('q');
+    window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    ew.style.marginLeft = value * -0.18 + 'px';
+    
+    })    
+    let qw = document.getElementById('qw');
+    window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    qw.style.marginRight = value * -0.2 + 'px';
+    
+    })    
+    let ed = document.getElementById('ws');
+    window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    ed.style.marginLeft = value * -0.18 + 'px';
+    
+    })    
+
+
+    document.getElementById("eo").style.opacity = '0';
+    $("#eo").animate({opacity:'1'}, 3000);
+
+    document.getElementById("er").style.opacity = '0';
+    $("#er").animate({opacity:'1'}, 3000);
+
+    document.getElementById("tr").style.opacity = '0';
+    $("#tr").animate({opacity:'1'}, 2000);
+
+    document.getElementById("ue").style.opacity = '0';
+    $("#ue").animate({opacity:'1'}, 2000);
